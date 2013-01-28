1ca049b52b80cee70c0cc4637a664801fb31d1d9
Alfred Assey M <asseym@gmail.com>
2013-01-28 16:30:28 +0300

    MEDIA_URL to STATIC_URL and other minor changes

--- poll/models.py
+++ poll/models.py
@@ -22,6 +22,7 @@ from generic.sorters import SimpleSorter
 from rapidsms.contrib.locations.models import Location
 from rapidsms.contrib.locations.nested import models as nested_models
 from rapidsms_httprouter.models import Message, MessageBatch
+# from rapidsms.contrib.messagelog.models import Message
 
 from django.conf import settings
 from django.db import models
